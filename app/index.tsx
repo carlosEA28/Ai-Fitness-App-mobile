0526399e7fd51f13cbecd00dedf6fb22c8d7563c
carlosEA28 <carloseaduarte28@gmail.com>
2025-08-06 11:32:41 -0300

    feat: add inital page

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,68 @@
-import { Image, Pressable, Text, View } from "react-native";
+import { colors } from "@/constants/colors";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 import React from "react";
 
 const Index = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Image source={require("@/assets/images/logo.png")} />
-      <Text>Dieta.IA</Text>
-      <Text>Sua dieta personalizada com inteligência artificial 🍴</Text>
-      <Pressable>
-        <Text>Gerar Dieta</Text>
+
+      <Text style={styles.title}>
+        Dieta
+        <Text style={{ color: colors.white }}>.IA</Text>
+      </Text>
+
+      <Text style={styles.text}>
+        Sua dieta personalizada com inteligência artificial 🍴
+      </Text>
+
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Gerar Dieta</Text>
       </Pressable>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.background,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    color: colors.green,
+  },
+
+  text: {
+    fontSize: 16,
+    color: colors.white,
+    width: 240,
+    textAlign: "center",
+    marginTop: 8,
+    marginBottom: 8,
+  },
+
+  button: {
+    backgroundColor: colors.blue,
+    width: "100%",
+    height: 40,
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 34,
+  },
+
+  buttonText: {
+    fontSize: 16,
+    color: colors.white,
+    fontWeight: "bold",
+  },
+});
 export default Index;
